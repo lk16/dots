4d8c87ae690a3a54934b0fc09b000f62dd57d8f4
Luuk Verweij <luuk_verweij@msn.com>
2017-04-03 09:35:08 +0200

    use stricter prototypes

--- bitset/bitset.go
+++ bitset/bitset.go
@@ -1,68 +1,73 @@
 package bitset
 
 import (
-    "math/rand"
+	"math/rand"
 )
 
 type Bitset uint64
 
 // Returns the number of set bits in a Bitset
-func (bs Bitset) Count() uint {
-    // TODO try this for potential better performance: https://github.com/barnybug/popcount
-    const (
-        m1  = 0x5555555555555555
-        m2  = 0x3333333333333333
-        m4  = 0x0f0f0f0f0f0f0f0f
-        h01 = 0x0101010101010101
-    )
-    bs -= (bs >> 1) & m1
-    bs = (bs & m2) + ((bs >> 2) & m2)
-    bs = (bs + (bs >> 4)) & m4
-    return uint((bs * h01) >> 56)
+func (bs Bitset) Count() (count uint) {
+	// TODO try this for potential better performance: https://github.com/barnybug/popcount
+	const (
+		m1  = 0x5555555555555555
+		m2  = 0x3333333333333333
+		m4  = 0x0f0f0f0f0f0f0f0f
+		h01 = 0x0101010101010101
+	)
+	bs -= (bs >> 1) & m1
+	bs = (bs & m2) + ((bs >> 2) & m2)
+	bs = (bs + (bs >> 4)) & m4
+	count = uint((bs * h01) >> 56)
+	return
 }
 
-// Tests if the bit in a Bitset at index is set 
+// Tests if the bit in a Bitset at index is set
 func (bs Bitset) TestBit(index uint) bool {
-    mask := Bitset(1 << index)
-    return bs & mask != 0
+	mask := Bitset(1 << index)
+	return bs&mask != 0
 }
 
 // Returns new bitset with only the first (least significant) bit set.
-// Returns the 0 Bitset if the input Bitset is 0 
-func (bs Bitset) FirstBit() Bitset {
-    return bs & -bs
+// Returns the 0 Bitset if the input Bitset is 0
+func (bs Bitset) FirstBit() (first_bit Bitset) {
+	first_bit = bs & -bs
+	return
 }
 
-// Returns index of first set bit in a Bitset 
+// Returns index of first set bit in a Bitset
 // Returns the 0 Bitset if the input Bitset is 0
-func (bs Bitset) FirstBitIndex() uint {
-    
-    magictable := [67]uint{
-         0,  0,  1, 39,  2, 15, 40, 23,
-         3, 12, 16, 59, 41, 19, 24, 54,
-         4,  0, 13, 10, 17, 62, 60, 28,
-        42, 30, 20, 51, 25, 44, 55, 47,
-         5, 32,  0, 38, 14, 22, 11, 58,
-        18, 53, 63,  9, 61, 27, 29, 50,
-        43, 46, 31, 37, 21, 57, 52,  8,
-        26, 49, 45, 36, 56,  7, 48, 35,
-         6, 34, 33}
+func (bs Bitset) FirstBitIndex() (first_index uint) {
 
-    return magictable[bs.FirstBit() % 67];
+	magictable := [67]uint{
+		0, 0, 1, 39, 2, 15, 40, 23,
+		3, 12, 16, 59, 41, 19, 24, 54,
+		4, 0, 13, 10, 17, 62, 60, 28,
+		42, 30, 20, 51, 25, 44, 55, 47,
+		5, 32, 0, 38, 14, 22, 11, 58,
+		18, 53, 63, 9, 61, 27, 29, 50,
+		43, 46, 31, 37, 21, 57, 52, 8,
+		26, 49, 45, 36, 56, 7, 48, 35,
+		6, 34, 33}
+
+	first_index = magictable[bs.FirstBit()%67]
+	return
 }
 
 // Returns last (most significant) set bit in a Bitset
 // Returns 0 Bitset if the input Bitset is 0
-func (bs Bitset) LastBit() Bitset {
-    for mask := Bitset(1 << 63); mask!=0; mask>>=1 {
-        if bs & mask != 0 {
-            return mask
-        }
-    }
-    return 0
+func (bs Bitset) LastBit() (last_bit Bitset) {
+	for mask := Bitset(1 << 63); mask != 0; mask >>= 1 {
+		if bs&mask != 0 {
+			last_bit = mask
+			return
+		}
+	}
+	return
 }
 
 // Returns a pseudo-random Bitset
-func RandomBitset() Bitset {
-    return Bitset(rand.Uint64())
+func RandomBitset() (random Bitset) {
+	random = Bitset(rand.Uint64())
+	return
 }
--- board/board.go
+++ board/board.go
@@ -1,181 +1,181 @@
 package board
 
 import (
-    "bytes"
-    "fmt"
-    "math/rand"
+	"bytes"
+	"fmt"
+	"math/rand"
 
-    "dots/bitset" 
+	"dots/bitset"
 )
 
 type Board struct {
-    me,opp bitset.Bitset
+	me, opp bitset.Bitset
 }
 
 // Returns a Board in start state
 func NewBoard() *Board {
-    return &Board{
-        me: bitset.Bitset(1 << 28) | bitset.Bitset(1 << 35),
-        opp: bitset.Bitset(1 << 27) | bitset.Bitset(1 << 36)}
+	return &Board{
+		me:  bitset.Bitset(1<<28) | bitset.Bitset(1<<35),
+		opp: bitset.Bitset(1<<27) | bitset.Bitset(1<<36)}
 }
 
 // Returns a deep copy of a Board
-func (board *Board) Clone() Board {
-    clone := *board
-    return clone
+func (board Board) Clone() (clone Board) {
+	clone = board
+	return
 }
 
 // Returns a String of ASCII-art of a Board
-func (board *Board) AsciiArt() string {
+func (board Board) AsciiArt() (output string) {
 
-    buffer := new(bytes.Buffer)
+	buffer := new(bytes.Buffer)
 
-    moves := board.Moves()
+	moves := board.Moves()
 
-    buffer.WriteString("+-a-b-c-d-e-f-g-h-+\n")
-    
-    for y:=uint(0); y<8; y++ {
+	buffer.WriteString("+-a-b-c-d-e-f-g-h-+\n")
 
-        buffer.WriteString(fmt.Sprintf("%d ",y+1))
+	for y := uint(0); y < 8; y++ {
 
-        for x:=uint(0); x<8; x++ {
-        
-            f := y*8 + x
+		buffer.WriteString(fmt.Sprintf("%d ", y+1))
 
-            if board.me.TestBit(f) {
-                buffer.WriteString("○ ")
-            } else if board.opp.TestBit(f) {
-                buffer.WriteString("● ")
-            } else if moves.TestBit(f) {
-                buffer.WriteString("- ")
-            } else {
-                buffer.WriteString("  ")
-            }
+		for x := uint(0); x < 8; x++ {
 
-        }
-     
-        buffer.WriteString("|\n")
-    }
-    buffer.WriteString("+-----------------+\n")
+			f := y*8 + x
 
+			if board.me.TestBit(f) {
+				buffer.WriteString("○ ")
+			} else if board.opp.TestBit(f) {
+				buffer.WriteString("● ")
+			} else if moves.TestBit(f) {
+				buffer.WriteString("- ")
+			} else {
+				buffer.WriteString("  ")
+			}
 
-    return buffer.String()
+		}
+
+		buffer.WriteString("|\n")
+	}
+	buffer.WriteString("+-----------------+\n")
+
+	output = buffer.String()
+	return
 }
 
 // Returns a subset of the moves for a Board
-func movesPartial(me,mask,n bitset.Bitset) bitset.Bitset {
-    flip_l := mask & (me << n)
-    flip_l |= mask & (flip_l << n)
-    mask_l := mask & (mask << n)
-    flip_l |= mask_l & (flip_l << (2*n))
-    flip_l |= mask_l & (flip_l << (2*n))
-    flip_r := mask & (me >> n)
-    flip_r |= mask & (flip_r >> n)
-    mask_r := mask & (mask >> n)
-    flip_r |= mask_r & (flip_r >> (2*n))
-    flip_r |= mask_r & (flip_r >> (2*n))
-    return (flip_l << n) | (flip_r >> n)
+func movesPartial(me, mask, n bitset.Bitset) (moves bitset.Bitset) {
+	flip_l := mask & (me << n)
+	flip_l |= mask & (flip_l << n)
+	mask_l := mask & (mask << n)
+	flip_l |= mask_l & (flip_l << (2 * n))
+	flip_l |= mask_l & (flip_l << (2 * n))
+	flip_r := mask & (me >> n)
+	flip_r |= mask & (flip_r >> n)
+	mask_r := mask & (mask >> n)
+	flip_r |= mask_r & (flip_r >> (2 * n))
+	flip_r |= mask_r & (flip_r >> (2 * n))
+	moves = (flip_l << n) | (flip_r >> n)
+	return
 }
 
 // Returns a Bitset with all valid moves for a Board
-func (board *Board) Moves() bitset.Bitset {
-    // this function is a modified version of code from Edax
-    mask := board.opp & 0x7E7E7E7E7E7E7E7E
+func (board Board) Moves() (moves bitset.Bitset) {
+	// this function is a modified version of code from Edax
+	mask := board.opp & 0x7E7E7E7E7E7E7E7E
 
-    res := movesPartial(board.me,mask,1)
-    res |= movesPartial(board.me,mask,7)
-    res |= movesPartial(board.me,mask,9)
-    res |= movesPartial(board.me,board.opp,8)
-    
-    empties := ^(board.me | board.opp)
+	moves = movesPartial(board.me, mask, 1)
+	moves |= movesPartial(board.me, mask, 7)
+	moves |= movesPartial(board.me, mask, 9)
+	moves |= movesPartial(board.me, board.opp, 8)
 
-    return res & empties
+	moves &^= (board.me | board.opp)
+	return
 }
 
 // Does the move at field index on a Board
 // Returns the flipped discs
-func (board *Board) DoMove(index uint) bitset.Bitset {
+func (board *Board) DoMove(index uint) (flipped bitset.Bitset) {
 
-    doMoveFuncs := []func() bitset.Bitset{
-         board.doMove0, board.doMove1, board.doMove2, board.doMove3,
-         board.doMove4, board.doMove5, board.doMove6, board.doMove7,
-         board.doMove8, board.doMove9,board.doMove10,board.doMove11,
-        board.doMove12,board.doMove13,board.doMove14,board.doMove15,
-        board.doMove16,board.doMove17,board.doMove18,board.doMove19,
-        board.doMove20,board.doMove21,board.doMove22,board.doMove23,
-        board.doMove24,board.doMove25,board.doMove26,board.doMove27,
-        board.doMove28,board.doMove29,board.doMove30,board.doMove31,
-        board.doMove32,board.doMove33,board.doMove34,board.doMove35,
-        board.doMove36,board.doMove37,board.doMove38,board.doMove39,
-        board.doMove40,board.doMove41,board.doMove42,board.doMove43,
-        board.doMove44,board.doMove45,board.doMove46,board.doMove47,
-        board.doMove48,board.doMove49,board.doMove50,board.doMove51,
-        board.doMove52,board.doMove53,board.doMove54,board.doMove55,
-        board.doMove56,board.doMove57,board.doMove58,board.doMove59,
-        board.doMove60,board.doMove61,board.doMove62,board.doMove63}
+	doMoveFuncs := []func() bitset.Bitset{
+		board.doMove0, board.doMove1, board.doMove2, board.doMove3,
+		board.doMove4, board.doMove5, board.doMove6, board.doMove7,
+		board.doMove8, board.doMove9, board.doMove10, board.doMove11,
+		board.doMove12, board.doMove13, board.doMove14, board.doMove15,
+		board.doMove16, board.doMove17, board.doMove18, board.doMove19,
+		board.doMove20, board.doMove21, board.doMove22, board.doMove23,
+		board.doMove24, board.doMove25, board.doMove26, board.doMove27,
+		board.doMove28, board.doMove29, board.doMove30, board.doMove31,
+		board.doMove32, board.doMove33, board.doMove34, board.doMove35,
+		board.doMove36, board.doMove37, board.doMove38, board.doMove39,
+		board.doMove40, board.doMove41, board.doMove42, board.doMove43,
+		board.doMove44, board.doMove45, board.doMove46, board.doMove47,
+		board.doMove48, board.doMove49, board.doMove50, board.doMove51,
+		board.doMove52, board.doMove53, board.doMove54, board.doMove55,
+		board.doMove56, board.doMove57, board.doMove58, board.doMove59,
+		board.doMove60, board.doMove61, board.doMove62, board.doMove63}
 
-    flipped := doMoveFuncs[index]()
+	flipped = doMoveFuncs[index]()
 
-    tmp := board.me | flipped | bitset.Bitset(1 << index)
+	tmp := board.me | flipped | bitset.Bitset(1<<index)
 
-    board.me = board.opp &^ tmp
-    board.opp = tmp
+	board.me = board.opp &^ tmp
+	board.opp = tmp
 
-    return flipped
+	return flipped
 }
 
 // Returns a channel that outputs the children of a Board
-func (board *Board) GenChildren() (ch chan Board) {
-    ch = make(chan Board)
-    go func() {
-        moves := board.Moves()
-        for {
-            index := moves.FirstBitIndex();
-            if index == uint(0) {
-                break
-            }
-            moves &^= bitset.Bitset(1<<index)
+func (board Board) GenChildren() (ch chan Board) {
+	ch = make(chan Board)
+	go func() {
+		moves := board.Moves()
+		for {
+			index := moves.FirstBitIndex()
+			if index == uint(0) {
+				break
+			}
+			moves &^= bitset.Bitset(1 << index)
 
-            clone := board.Clone()
-            clone.DoMove(index)
-            ch <- clone
-        }
-        close(ch)
-    }()
-    return ch
+			clone := board.Clone()
+			clone.DoMove(index)
+			ch <- clone
+		}
+		close(ch)
+	}()
+	return
 }
 
 // Returns a slice with all children of a Board
-func (board *Board) GetChildren() (children []Board) {
-    for child := range board.GenChildren() {
-        children = append(children,child)
-    }
-    return
+func (board Board) GetChildren() (children []Board) {
+	for child := range board.GenChildren() {
+		children = append(children, child)
+	}
+	return
 }
 
-func (board Board) doRandomMoves(move_count,skips uint) Board {
+// Does n random moves on a Board
+func (board *Board) DoRandomMoves(n uint) {
 
-    if (skips == 2) || (move_count == 0) {
-        return board
-    }
+	skips := 0
 
-    children := board.GetChildren()
+	for (n > 0) && (skips < 2) {
 
-    if len(children) == 0 {
-        board.SwitchTurn()
-        return board.doRandomMoves(move_count,skips + 1)
-    }
+		children := board.GetChildren()
+		if len(children) == 0 {
+			board.SwitchTurn()
+			skips++
+			continue
+		}
 
-    child := children[rand.Int() % len(children)]
-    return child.doRandomMoves(move_count-1,0)
+		*board = children[rand.Int()%len(children)]
+		n--
+		skips = 0
+	}
 }
 
-func (board Board) DoRandomMoves(move_count uint) Board {
-    return board.doRandomMoves(move_count,0)
-}
-
+// Switches turn of a Board
 func (board *Board) SwitchTurn() {
-    tmp := board.me
-    board.me = board.opp
-    board.opp = tmp
+	tmp := board.me
+	board.me = board.opp
+	board.opp = tmp
 }
