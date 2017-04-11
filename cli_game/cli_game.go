27c1abc730a0439b4183596d81791b74d608756d
Luuk Verweij <luuk_verweij@msn.com>
2017-04-11 14:33:07 +0200

    added CliGame.ResultString() and one unit test

--- cli_game/cli_game.go
+++ cli_game/cli_game.go
@@ -55,17 +55,23 @@ func (cli *CliGame) Run() {
 	if cli.turn == 1 {
 		cli.board.SwitchTurn()
 	}
+
+	fmt.Printf("%s\n", cli.ResultString())
+
+}
+
+func (cli CliGame) ResultString() (str string) {
 	white_count := cli.board.Opp().Count()
 	black_count := cli.board.Me().Count()
 
 	if white_count > black_count {
-		fmt.Printf("White wins: %d-%d\n", white_count, black_count)
+		str = fmt.Sprintf("White wins: %d-%d\n", white_count, black_count)
 	} else if white_count < black_count {
-		fmt.Printf("Black wins: %d-%d\n", black_count, white_count)
+		str = fmt.Sprintf("Black wins: %d-%d\n", black_count, white_count)
 	} else {
-		fmt.Printf("It's a draw: %d-%d\n", white_count, white_count)
+		str = fmt.Sprintf("It's a draw: %d-%d\n", white_count, white_count)
 	}
-
+	return
 }
 
 // Returns a string with ascii-art representing the current board state
