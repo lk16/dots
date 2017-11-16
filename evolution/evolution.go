67a9b2471481ad2e6a1c14ff2e90c8b4e1e00513
Luuk Verweij <luuk_verweij@msn.com>
2017-11-16 19:51:55 +0100

    fixed concurrent map modification bug

--- evolution/evolution.go
+++ evolution/evolution.go
@@ -11,6 +11,7 @@ import (
 	"math/rand"
 	"os"
 	"os/signal"
+	"runtime"
 	"sort"
 	"syscall"
 	"time"
@@ -18,16 +19,13 @@ import (
 
 const (
 	maxParticipants = 40
-	WHITE           = 0
-	DRAW            = 1
-	BLACK           = 2
 	startRating     = 1500
 	paramMax        = 10000
 )
 
 type Participant struct {
 	Params players.Parameters
-	rating int
+	Rating int
 	Wins   int
 	Losses int
 	Draws  int
@@ -36,7 +34,7 @@ type Participant struct {
 func NewParticipantRandom(min, max int) (particpant Participant) {
 	return Participant{
 		Params: players.RandomParameters(min, max),
-		rating: startRating}
+		Rating: startRating}
 }
 
 func NewParticipantMutate(parent Participant) (offspring Participant) {
@@ -47,36 +45,36 @@ func NewParticipantMutate(parent Participant) (offspring Participant) {
 			offspring.Params.PositionValue[i] += rand.Intn(500) - rand.Intn(500)
 		}
 	}
-	offspring.rating = startRating
+	offspring.Rating = startRating
 	return
 }
 
 type result struct {
-	blackId int
-	whiteId int
-	winner  int
+	blackID    int
+	whiteID    int
+	blackScore float64
 }
 
 // Evolution runs an evolutionary algorithm
 type Evolution struct {
-	killChan     chan bool
-	resultChan   chan result
-	participants []Participant
-	filename     string
-	searchDepth  int
-	exactDepth   int
-	runningGames int
-	launchesLeft int
+	killChan          chan bool
+	resultChan        chan result
+	participants      map[int]Participant
+	filename          string
+	searchDepth       int
+	exactDepth        int
+	nextParticipantID int
 }
 
 // NewEvolution creates a new Evolution
 func NewEvolution(filename string) (evolution *Evolution) {
 	evolution = &Evolution{
-		killChan:    make(chan bool),
-		resultChan:  make(chan result),
-		filename:    filename,
-		searchDepth: 4,
-		exactDepth:  8}
+		killChan:     make(chan bool),
+		resultChan:   make(chan result),
+		filename:     filename,
+		searchDepth:  4,
+		exactDepth:   8,
+		participants: make(map[int]Participant, 0)}
 
 	signals := make(chan os.Signal)
 
@@ -88,16 +86,17 @@ func NewEvolution(filename string) (evolution *Evolution) {
 	}()
 
 	evolution.Load()
-	for len(evolution.participants) < maxParticipants {
-		evolution.participants = append(evolution.participants, NewParticipantRandom(-paramMax, paramMax))
-	}
+
 	return
 }
 
-func (evolution *Evolution) playGame(blackId, whiteId int, b board.Board) {
+func playGame(
+	blackID int, blackParams players.Parameters,
+	whiteID int, whiteParams players.Parameters,
+	searchDepth, exactDepth int, b board.Board, ch chan result) {
 	players := []players.Player{
-		players.NewBotEvolve(evolution.searchDepth, evolution.exactDepth, evolution.participants[blackId].Params),
-		players.NewBotEvolve(evolution.searchDepth, evolution.exactDepth, evolution.participants[whiteId].Params)}
+		players.NewBotEvolve(searchDepth, exactDepth, blackParams),
+		players.NewBotEvolve(searchDepth, exactDepth, whiteParams)}
 
 	turn := 0
 
@@ -115,11 +114,6 @@ func (evolution *Evolution) playGame(blackId, whiteId int, b board.Board) {
 		b = players[turn].DoMove(b)
 	}
 
-	r := result{
-		blackId: blackId,
-		whiteId: whiteId,
-	}
-
 	if turn == 1 {
 		b.SwitchTurn()
 	}
@@ -127,123 +121,186 @@ func (evolution *Evolution) playGame(blackId, whiteId int, b board.Board) {
 	blackCount := bits.OnesCount64(b.Me())
 	whiteCount := bits.OnesCount64(b.Opp())
 
+	r := result{
+		blackID: blackID,
+		whiteID: whiteID}
+
 	if blackCount < whiteCount {
-		r.winner = WHITE
+		r.blackScore = 0
 	} else if blackCount > whiteCount {
-		r.winner = BLACK
+		r.blackScore = 1
 	} else {
-		r.winner = DRAW
+		r.blackScore = 0.5
 	}
 
-	evolution.resultChan <- r
+	ch <- r
 }
 
-func (evolution *Evolution) launchGames() {
-	for i := range evolution.participants {
-		j := rand.Intn(len(evolution.participants))
-		if i != j {
-			evolution.participants[i], evolution.participants[j] = evolution.participants[j], evolution.participants[i]
-		}
+func (evolution *Evolution) launchGame() {
+
+	a := rand.Intn(len(evolution.participants))
+	var b int
+
+	for a == b {
+		b = rand.Intn(len(evolution.participants))
 	}
 
-	for g := 0; g < len(evolution.participants)/2; g++ {
-		xot := board.Xot()
-		go evolution.playGame(2*g, (2*g)+1, xot)
+	var blackID, whiteID int
+
+	for ID := range evolution.participants {
+		if a == 0 {
+			blackID = ID
+		}
+		if b == 0 {
+			whiteID = ID
+		}
+		a--
+		b--
 	}
-	evolution.runningGames = len(evolution.participants) / 2
-	evolution.launchesLeft--
+
+	xot := board.Xot()
+	blackParams := evolution.participants[blackID].Params
+	whiteParams := evolution.participants[whiteID].Params
+
+	go playGame(blackID, blackParams, whiteID, whiteParams,
+		evolution.searchDepth, evolution.exactDepth, xot,
+		evolution.resultChan)
+}
+
+func (evolution *Evolution) printStats() {
+	participantsCopy := []Participant{}
+	for _, v := range evolution.participants {
+		participantsCopy = append(participantsCopy, v)
+	}
+	sort.Slice(participantsCopy, func(i, j int) bool {
+		return participantsCopy[i].Rating > participantsCopy[j].Rating
+	})
+
+	fmt.Printf("Rating\tGames\tWins\tLosses\tDraws\tWin Rate\tParams\n")
+	for _, p := range participantsCopy {
+		games := p.Draws + p.Wins + p.Losses
+		winRate := 100.0 * float64(p.Wins) / float64(games)
+		fmt.Printf("%d\t%d\t%d\t%d\t%d\t%2.2f%%\t%v\n", p.Rating, games, p.Wins, p.Losses, p.Draws, winRate, p.Params)
+	}
+	fmt.Printf("\n\n")
 }
 
 // Run runs evolutionary algorithm
 func (evolution *Evolution) Run() {
-	for {
-		timeoutChan := make(chan bool)
+	timeoutChan := make(chan bool)
 
-		go func() {
-			for {
-				time.Sleep(1 * time.Second)
-				timeoutChan <- true
-			}
-		}()
-
-		evolution.launchesLeft = 10
-		evolution.launchGames()
-
+	go func() {
 		for {
-			select {
-			case <-evolution.killChan:
-				fmt.Printf("\nCleaning up\n")
-				evolution.Save()
-				return
-			case r := <-evolution.resultChan:
-				score := float64(r.winner) / 2
-				ratingDiff := evolution.participants[r.whiteId].rating - evolution.participants[r.blackId].rating
-				expected := 1.0 / (1.0 + (math.Pow(10.0, (float64(ratingDiff) / 400.0))))
-				change := int(32.0 * (score - expected))
-				evolution.participants[r.blackId].rating += change
-				evolution.participants[r.whiteId].rating -= change
-				if r.winner == WHITE {
-					evolution.participants[r.whiteId].Wins++
-					evolution.participants[r.blackId].Losses++
-				} else if r.winner == DRAW {
-					evolution.participants[r.whiteId].Draws++
-					evolution.participants[r.blackId].Draws++
-				} else {
-					evolution.participants[r.whiteId].Losses++
-					evolution.participants[r.blackId].Wins++
-				}
+			time.Sleep(1 * time.Second)
+			timeoutChan <- true
+		}
+	}()
 
-				evolution.runningGames--
-				if evolution.runningGames == 0 {
+	for i := 0; i < runtime.NumCPU(); i++ {
+		evolution.launchGame()
+	}
 
-					if evolution.launchesLeft == 0 {
-						sort.Slice(evolution.participants, func(i, j int) bool {
-							return evolution.participants[i].rating > evolution.participants[j].rating
-						})
-
-						removedParticipantsCount := len(evolution.participants) / 2
-						removedParticipantsStart := len(evolution.participants) - removedParticipantsCount
-						newRandomParticipantsStart := len(evolution.participants) - (removedParticipantsCount / 3)
-
-						for i := removedParticipantsStart; i < len(evolution.participants); i++ {
-							if i >= newRandomParticipantsStart {
-								evolution.participants[i] = NewParticipantRandom(-paramMax, paramMax)
-							} else {
-								evolution.participants[i] = NewParticipantMutate(evolution.participants[rand.Intn(removedParticipantsStart)])
-							}
-							evolution.participants[i].Draws = 0
-							evolution.participants[i].Wins = 0
-							evolution.participants[i].Losses = 0
-						}
-
-						for i := range evolution.participants {
-							evolution.participants[i].rating = startRating
-						}
-
-						evolution.launchesLeft = 10
-					}
-					evolution.launchGames()
-				}
-				break
-			case <-timeoutChan:
-				participantsCopy := make([]Participant, len(evolution.participants))
-				copy(participantsCopy, evolution.participants)
-				sort.Slice(participantsCopy, func(i, j int) bool {
-					return participantsCopy[i].rating > participantsCopy[j].rating
-				})
-
-				fmt.Printf("%d launches left.\n", evolution.launchesLeft)
-				fmt.Printf("%d games running.\n", evolution.runningGames)
-				fmt.Printf("Rating\tGames\tWins\tLosses\tDraws\tWin Rate\tParams\n")
-				for _, p := range participantsCopy {
-					games := p.Draws + p.Wins + p.Losses
-					winRate := 100.0 * float64(p.Wins) / float64(games)
-					fmt.Printf("%d\t%d\t%d\t%d\t%d\t%2.2f%%\t%v\n", p.rating, games, p.Wins, p.Losses, p.Draws, winRate, p.Params)
-				}
-				fmt.Printf("\n\n")
-			}
+	for {
+		select {
+		case <-evolution.killChan:
+			fmt.Printf("\nCleaning up\n")
+			evolution.Save()
+			return
+		case r := <-evolution.resultChan:
+			evolution.updateRating(r)
+			evolution.launchGame()
+			break
+		case <-timeoutChan:
+			evolution.replaceParticipants()
+			evolution.printStats()
 		}
 	}
+}
+
+func (evolution *Evolution) replaceParticipants() {
+
+	deadParticipants := []int{}
+	inflationTotal := 0
+
+	for ID, p := range evolution.participants {
+		if p.Rating > startRating {
+			continue
+		}
+
+		games := p.Draws + p.Losses + p.Draws
+		if p.Rating < (startRating-300)+(20*games) {
+			deadParticipants = append(deadParticipants, ID)
+			inflationTotal += (startRating - p.Rating)
+		}
+	}
+
+	for _, ID := range deadParticipants {
+		delete(evolution.participants, ID)
+	}
+
+	for _ = range deadParticipants {
+		var newParticipant Participant
+		if rand.Intn(3) == 0 {
+			newParticipant = NewParticipantRandom(-paramMax, paramMax)
+		} else {
+			n := rand.Intn(len(evolution.participants))
+			var parent Participant
+			for _, parent = range evolution.participants {
+				if n == 0 {
+					break
+				}
+			}
+			newParticipant = NewParticipantMutate(parent)
+		}
+		newParticipant.Rating = startRating
+		evolution.participants[evolution.nextParticipantID] = newParticipant
+		evolution.nextParticipantID++
+	}
+
+	i := 0
+	for ID, p := range evolution.participants {
+		p.Rating -= inflationTotal / len(evolution.participants)
+		if i < inflationTotal%len(evolution.participants) {
+			p.Rating--
+		}
+		evolution.participants[ID] = p
+
+		i++
+	}
+}
+
+func (evolution *Evolution) updateRating(r result) {
+	RatingDiff := evolution.participants[r.whiteID].Rating - evolution.participants[r.blackID].Rating
+	expected := 1.0 / (1.0 + (math.Pow(10.0, (float64(RatingDiff) / 400.0))))
+	blackChange := int(32.0 * (r.blackScore - expected))
+
+	var whiteParticipant, blackParticipant Participant
+	var ok bool
+
+	if whiteParticipant, ok = evolution.participants[r.whiteID]; !ok {
+		fmt.Printf("One of the participants doesn't exist anymore.\n")
+		return
+	}
+	if blackParticipant, ok = evolution.participants[r.blackID]; !ok {
+		fmt.Printf("One of the participants doesn't exist anymore.\n")
+		return
+	}
+
+	blackParticipant.Rating += blackChange
+	whiteParticipant.Rating -= blackChange
+	if r.blackScore == 1.0 {
+		blackParticipant.Wins++
+		whiteParticipant.Losses++
+	} else if r.blackScore == 0.5 {
+		blackParticipant.Draws++
+		whiteParticipant.Draws++
+	} else {
+		blackParticipant.Losses++
+		whiteParticipant.Wins++
+	}
+
+	evolution.participants[r.blackID] = blackParticipant
+	evolution.participants[r.whiteID] = whiteParticipant
 }
 
 // Save saves the state of Evolution
@@ -252,19 +309,38 @@ func (evolution *Evolution) Save() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	json.NewEncoder(file).Encode(evolution.participants)
+	var ps []Participant
+	for _, p := range evolution.participants {
+		ps = append(ps, p)
+	}
+	json.NewEncoder(file).Encode(ps)
 }
 
 // Load loads the state of Evolution
 func (evolution *Evolution) Load() {
 	file, err := os.Open(evolution.filename)
-	if err != nil {
+	if err == nil {
+		var ps []Participant
+		json.NewDecoder(file).Decode(&ps)
+
+		evolution.participants = make(map[int]Participant, len(ps))
+		for i, p := range ps {
+			p.Rating = startRating
+			evolution.participants[i] = p
+		}
+	} else {
 		fmt.Printf("Could not load state: %s", err)
-		return
 	}
-	json.NewDecoder(file).Decode(&evolution.participants)
-	for i := range evolution.participants {
-		evolution.participants[i].rating = startRating
+
+	evolution.nextParticipantID = len(evolution.participants)
+	if len(evolution.participants) < maxParticipants {
+		fmt.Printf("Adding random initial participants\n")
+		for len(evolution.participants) < maxParticipants {
+			p := NewParticipantRandom(-paramMax, paramMax)
+			p.Rating = startRating
+			evolution.participants[evolution.nextParticipantID] = p
+			evolution.nextParticipantID++
+		}
 	}
-	fmt.Printf("Loaded state\n\n")
+
 }
