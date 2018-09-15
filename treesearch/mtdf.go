7154f6427111d00bfa970285ffd8d88e9692c298
Luuk Verweij <luuk_verweij@msn.com>
2018-09-15 15:28:18 +0200

    mtdf with lame hashtable implemented

--- treesearch/mtdf.go
+++ treesearch/mtdf.go
@@ -105,6 +105,148 @@ func (mtdf *Mtdf) search(alpha int) int {
 			return alpha + 1
 		}
 	}
+	return alpha
+}
 
+// -----
+
+type bounds struct {
+	upper int
+	lower int
+}
+
+type MtdfHashTable struct {
+	board  othello.Board
+	high   int
+	low    int
+	depth  int
+	lookup map[othello.Board]bounds
+}
+
+func NewMtdfHashTable(low, high int) *MtdfHashTable {
+	return &MtdfHashTable{
+		high: high,
+		low:  low}
+}
+
+func (mtdf *MtdfHashTable) Name() string {
+	return "mtdfhashtable"
+}
+
+func (mtdf *MtdfHashTable) Search(board othello.Board, depth int) int {
+	mtdf.board = board
+	mtdf.depth = depth
+	mtdf.lookup = make(map[othello.Board]bounds, 10240)
+	return mtdf.slideWindow()
+}
+
+func (mtdf *MtdfHashTable) ExactSearch(board othello.Board) int {
+	return mtdf.Search(board, 60)
+}
+
+func (mtdf *MtdfHashTable) slideWindow() int {
+
+	f := heuristics.Squared(mtdf.board)
+
+	var step int
+	if mtdf.board.CountEmpties() > mtdf.depth {
+		step = 1
+	} else {
+		step = 2
+	}
+
+	// prevent odd results for exact search
+	f -= f % step
+
+	if f < mtdf.low {
+		f = mtdf.low
+	}
+
+	if f > mtdf.high {
+		f = mtdf.high
+	}
+
+	for mtdf.high-mtdf.low >= step {
+		var bound = mtdf.search(f)
+
+		if f == bound {
+			f -= step
+			mtdf.high = bound
+		} else {
+			f += step
+			mtdf.low = bound
+		}
+	}
+
+	return mtdf.high
+}
+
+func (mtdf *MtdfHashTable) polish(heur, alpha int) int {
+	if heur > alpha {
+		return alpha + 1
+	}
+	return alpha
+}
+
+func (mtdf *MtdfHashTable) search(alpha int) (heur int) {
+
+	defer func() {
+
+		if mtdf.depth >= 3 {
+			entry, ok := mtdf.lookup[mtdf.board.Normalize()]
+
+			if !ok {
+				entry = bounds{
+					lower: MinHeuristic,
+					upper: MaxHeuristic}
+			}
+
+			if heur == alpha && heur < entry.upper {
+				entry.upper = alpha
+			} else if heur > entry.lower {
+				entry.lower = alpha + 1
+			}
+			mtdf.lookup[mtdf.board] = entry
+		}
+	}()
+
+	if mtdf.depth >= 3 {
+		if entry, ok := mtdf.lookup[mtdf.board.Normalize()]; ok {
+			if entry.upper <= alpha {
+				return alpha
+			}
+			if entry.lower >= alpha+1 {
+				return alpha + 1
+			}
+		}
+	}
+
+	if mtdf.depth == 0 {
+		return mtdf.polish(heuristics.Squared(mtdf.board), alpha)
+	}
+
+	gen := othello.NewGenerator(&mtdf.board, 0)
+
+	if !gen.HasMoves() {
+
+		if mtdf.board.OpponentMoves() != 0 {
+			mtdf.board.SwitchTurn()
+			heur = -mtdf.search(-(alpha + 1))
+			mtdf.board.SwitchTurn()
+			return
+		}
+
+		return mtdf.polish(ExactScoreFactor*mtdf.board.ExactScore(), alpha)
+	}
+
+	for gen.Next() {
+		mtdf.depth--
+		childHeur := -mtdf.search(-(alpha + 1))
+		mtdf.depth++
+		if childHeur > alpha {
+			gen.RestoreParent()
+			return alpha + 1
+		}
+	}
 	return alpha
 }
--- treesearch/treesearch_test.go
+++ treesearch/treesearch_test.go
@@ -9,55 +9,63 @@ import (
 
 func TestTreeSearch(t *testing.T) {
 
-	for depth := 0; depth <= 4; depth++ {
-		for discs := 4; discs <= 64; discs++ {
-			for i := 0; i <= 10; i++ {
+	internal := func(t *testing.T, discs, depth int) {
+		algos := []Interface{
+			NewMinimax(),
+			NewNegaMax(),
+			NewAlphaBeta(MinHeuristic, MaxHeuristic),
+			NewMtdf(MinHeuristic, MaxHeuristic),
+			NewMtdfHashTable(MinHeuristic, MaxHeuristic)}
+		board := othello.RandomBoard(discs)
 
-				algos := []Interface{
-					NewMinimax(),
-					NewNegaMax(),
-					NewAlphaBeta(MinHeuristic, MaxHeuristic),
-					NewMtdf(MinHeuristic, MaxHeuristic)}
-				board := othello.RandomBoard(discs)
+		results := make(map[string]int, len(algos))
 
-				results := make(map[string]int, len(algos))
+		for _, algo := range algos {
+			if (algo.Name() == "minimax" || algo.Name() == "negamax") && depth >= 4 {
+				continue
+			}
 
-				for _, algo := range algos {
-					if (algo.Name() == "minimax" || algo.Name() == "negamax") && depth >= 4 {
-						continue
-					}
+			result := algo.Search(*board, depth)
 
-					result := algo.Search(*board, depth)
+			if result <= -200 || result >= 200 {
+				// skip exact search
+				break
+			}
+			results[algo.Name()] = result
+		}
 
-					if result <= -200 || result >= 200 {
-						// skip exact search
-						break
-					}
-					results[algo.Name()] = result
-				}
+		resultsSet := make(map[int]struct{}, len(algos))
 
-				resultsSet := make(map[int]struct{}, len(algos))
+		for _, result := range results {
+			resultsSet[result] = struct{}{}
+		}
 
-				for _, result := range results {
-					resultsSet[result] = struct{}{}
-				}
-
-				if len(resultsSet) > 1 {
-					msg := "Found inconsistent tree search results:\n"
-					for _, algo := range algos {
-						if result, ok := results[algo.Name()]; ok {
-							msg += fmt.Sprintf("%s: %d\n", algo.Name(), result)
-						} else {
-							msg += fmt.Sprintf("%s: <skipped>\n", algo.Name())
-						}
-					}
-					var buff bytes.Buffer
-					board.ASCIIArt(&buff, false)
-					msg += fmt.Sprintf("for this board at depth %d:\n\n%s", depth, buff.String())
-					t.Error(msg)
-					t.FailNow()
+		if len(resultsSet) > 1 {
+			msg := "Found inconsistent tree search results:\n"
+			for _, algo := range algos {
+				if result, ok := results[algo.Name()]; ok {
+					msg += fmt.Sprintf("%s: %d\n", algo.Name(), result)
+				} else {
+					msg += fmt.Sprintf("%s: <skipped>\n", algo.Name())
 				}
 			}
+			var buff bytes.Buffer
+			board.ASCIIArt(&buff, false)
+			msg += fmt.Sprintf("for this board at depth %d:\n\n%s", depth, buff.String())
+			t.Error(msg)
+			t.FailNow()
 		}
+	}
+
+	for depth := 0; depth <= 4; depth++ {
+		for discs := 4; discs <= 64; discs++ {
+			for i := 0; i <= 5; i++ {
+				internal(t, discs, depth)
+			}
+		}
+	}
+
+	for i := 0; i < 20; i++ {
+		internal(t, 8, 6)
 	}
 }
