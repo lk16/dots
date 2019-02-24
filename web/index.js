8f008a2ab1a1c1306d3cc6fa882f2874fcc2261e
Luuk Verweij <luuk_verweij@msn.com>
2019-02-24 21:22:23 +0100

    no-gap table

--- web/index.js
+++ web/index.js
@@ -1,11 +1,13 @@
 $(function(){
     for(let y=0; y<8;y++){
         let row = $("<tr></tr>");
+        $("#board").append(row);
         for (let x=0; x<8; x++){
             $(row, "tr").append("<td></td>");
         }
-        $("#board").append(row);
     }
+
+    $('#board td').append('<img src="empty.png" />');
 });
 
 $(document).on("click", "#board td", function () {
