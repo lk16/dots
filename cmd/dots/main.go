615e684d8fc60d1f9b386908d452c03d20f9922e
Luuk Verweij <luuk_verweij@msn.com>
2020-04-19 14:14:48 +0200

    refactor: remove junk from cmd/dots/main.go

--- cmd/dots/main.go
+++ cmd/dots/main.go
@@ -1,77 +1,9 @@
 package main
 
 import (
-	"flag"
-	"github.com/lk16/dots/internal/othello"
-	"github.com/lk16/dots/internal/treesearch"
 	"github.com/lk16/dots/internal/web"
-	"io/ioutil"
-	"log"
-	"math/rand"
-	"os"
-	"runtime/pprof"
-	"time"
 )
 
-func profile(cpuprofile string, profiled func()) {
-
-	f, err := os.Create(cpuprofile)
-	if err != nil {
-		log.Fatalf("Error creating profiling file: %s", err)
-	}
-
-	err = pprof.StartCPUProfile(f)
-	if err != nil {
-		log.Fatalf("Profiling error: %s", err)
-	}
-	defer pprof.StopCPUProfile()
-
-	profiled()
-}
-
-func devMain() {
-	log.Printf("devMain() running")
-}
-
 func main() {
-
-	defaultSeed := time.Now().UTC().UnixNano()
-	seed := flag.Int64("seed", defaultSeed, "Custom seed")
-
-	cpuprofile := flag.String("cpuprofile", "", "write cpu profile to file")
-
-	dev := flag.Bool("dev", false, "run devMain()")
-
-	flag.Parse()
-
-	rand.Seed(*seed)
-
-	if *dev {
-		devMain()
-		return
-	}
-
-	if *cpuprofile != "" {
-		profile(*cpuprofile, func() {
-			rand.Seed(0)
-			exactDepth := 16
-			bot := treesearch.NewBot(ioutil.Discard, 3, exactDepth)
-			for i := 0; i < 10; i++ {
-				board := othello.NewXotBoard()
-				for board.CountEmpties() >= exactDepth {
-					var err error
-					board, err = bot.DoMove(*board)
-					if err != nil {
-						log.Printf("error: %s", err)
-						return
-					}
-				}
-				log.Printf("bot lifetime stats: %s\n", bot.LifetimeStats.String())
-			}
-		})
-		return
-	}
-
 	web.Main()
-
 }
